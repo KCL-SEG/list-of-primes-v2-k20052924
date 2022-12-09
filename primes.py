03cef55f13da744632a03aeef6f930f0c596ef41
k20052924 <k20052924@kcl.ac.uk>
2022-12-09 13:33:01 +0000

    inital

--- primes.py
+++ primes.py
@@ -3,4 +3,14 @@
 
 def primes(number_of_primes):
     list = []
+    for number in range (0, number_of_primes+1):
+        prime = True
+        if number > 1:
+            prime = False
+            for i in range (2, number):
+                if (number % i) == 0:
+                    prime = False
+            if prime:
+                list.append(number)
+    print(list)
     return list
